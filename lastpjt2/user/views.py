fb7df43bae59e74a64f14d78fdf22a7a69354278
park3773 <park3773.sh@gmail.com>
2022-08-28 18:24:05 +0900

    start webproject
    django ==3.2

---
+++ lastpjt2/user/views.py
@@ -0,0 +1,56 @@
+from django.contrib.auth import get_user_model, login, logout
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+from .forms import LoginForm, RegisterForm
+
+User = get_user_model()
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/login")
+    else:
+        logout(request)
+        form = RegisterForm()
+    return render(request, "register.html", {"form": form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            user = User.objects.get(username=username)
+            login(request, user)
+            request.session["user"] = username
+            return HttpResponseRedirect("/")
+    else:
+        form = LoginForm()
+    return render(request, "login.html", {"form": form})
+
+
+def logout_view(request):
+    if "user" in request.session:
+        del request.session["user"]
+    logout(request)
+    return HttpResponseRedirect("/")
+
+
+@login_required(login_url="/login")
+def user_list_view(request):
+    page = int(request.GET.get("page", 1))
+    users = User.objects.all().order_by("-id")
+    paginator = Paginator(users, 3)
+    users = paginator.get_page(page)
+
+    return render(request, "users.html", {"users": users})
